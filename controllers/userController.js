e312540274ffc745dfca3f1c0fd97e5a4b5802b9
MohitSingh <mohitsingh540zs@gmail.com>
2026-07-20 15:04:16 +0530

    Added login controller, route and jwt config

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import generateToken from "../utils/generateToken.js";
 
 
 export const register = async (req, res) => {
@@ -10,7 +11,7 @@ export const register = async (req, res) => {
         const user = await User.findOne({ email });
 
         if (user) {
-            return res.status(400).json({
+            return res.status(409).json({
                 success: false,
                 message: "User already exists, please login."
             });
@@ -25,10 +26,65 @@ export const register = async (req, res) => {
             password: hashPassword
         });
 
+        const safeuser = user.toObject();
+        delete safeuser.password;
+
         return res.status(201).json({
             success: true,
             message: "User created successfully",
             newUser
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message || "Internal Server Error"
+        });
+    }
+}
+
+export const login = async (req, res) => {
+
+    const { email, password } = req.body;
+
+    try {
+        // basic validation
+
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            });
+        }
+
+
+        const user = await User.findOne({ email }).select("+password");
+
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid email or password"
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid credentials"
+            });
+        }
+
+        const token = generateToken(user._id);
+        const safeUser = user.toObject();
+        delete safeUser.password;
+
+        return res.status(200).json({
+            success: true,
+            message: `Welcome back ${user.firstName}`,
+            user: safeUser,
+            token
         });
 
     } catch (error) {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { register } from "../controllers/userController.js";
+import validator from "../middleware/validator.js";
 
 const userRoutes = express.Router();
 
 // userRoutes.get('/all-users', getAllUsers);
-userRoutes.post('/register', register);
+userRoutes.post('/register', validator, register);
 
 export default userRoutes
